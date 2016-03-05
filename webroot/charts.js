2bd916c4d3571faeac163a6149d1996e02a10187
Justina Mickonyte <j.mickonyte@gmail.com>
2016-03-05 17:31:30 +0200

    temperature data displayed in a chart

--- webroot/charts.js
+++ webroot/charts.js
@@ -6,7 +6,36 @@ function fillChartData(chartId, data)
 
 	obj = JSON.parse(data);
 
-	var test = obj.value.time;
-	test = test.toString()
-	chartElement.innerHTML = test;
-}
+	var time = obj.value.time;
+	var value = obj.value.value;
+
+	var data = [];	
+	var txtData = '[';
+	for(i = 0; i < time.length; i++){
+		text = '{"time": "' + time[i] + '",\n"value": ' + value[i] + '}';
+		txtData += text;
+		if(i < time.length-1) 
+			txtData += ',';
+	}
+
+	txtData += ']';
+//	chartElement.innerHTML = txtData;
+	data = JSON.parse(txtData);
+	
+	MG.data_graphic({
+        title: chartId,
+        description: chartId,
+        data: data,
+        width: 400,
+        height: 200,
+        right: 40,
+        target: chartElement,
+	    x_accessor: 'time',
+        y_accessor: 'value'
+    });
+
+//	var test = obj.value.value;
+
+	}
+
+fillChartData("temperatureChart", "exampleData.json");
